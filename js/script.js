9ed11225171379d48dfe1bd613265b827010e1ac
Maisdesign <downloadtaky@gmail.com>
2025-12-10 23:37:19 +0100

    Creata funzione per rendere il codice più efficiente

--- js/script.js
+++ js/script.js
@@ -1,31 +1,25 @@
 const resultDiv = document.getElementById('result');
+function createLiElement(content, styleClasses) {
+    let nuovoLi = document.createElement('li');
+    nuovoLi.classList.add('list-group-item', ...styleClasses);
+    nuovoLi.textContent = content;
+    return nuovoLi;
+};
 
 for (let index = 1; index <= 100; index = index + 1) {
     // Parto dal caso più specifico
     if (index % 3 === 0 && index % 5 === 0) {
         console.log("FizzBuzz");
-        let nuovoLi = document.createElement('li');
-        nuovoLi.classList.add('list-group-item', 'list-group-item-danger');
-        nuovoLi.innerHTML = `FizzBuzz`;
-        resultDiv.appendChild(nuovoLi);
+        resultDiv.appendChild(createLiElement('FizzBuzz', ['list-group-item-danger']));
     } else if (index % 3 === 0) {
         console.log("Fizz");
-        let nuovoLi = document.createElement('li');
-        nuovoLi.classList.add('list-group-item', 'list-group-item-success');
-        nuovoLi.innerHTML = `Fizz`;
-        resultDiv.appendChild(nuovoLi);
+        resultDiv.appendChild(createLiElement('Fizz', ['list-group-item-success']));
     } else if (index % 5 === 0) {
         console.log("Buzz");
-        let nuovoLi = document.createElement('li');
-        nuovoLi.classList.add('list-group-item', 'list-group-item-info');
-        nuovoLi.innerHTML = `Buzz`;
-        resultDiv.appendChild(nuovoLi);
+        resultDiv.appendChild(createLiElement('Buzz', ['list-group-item-info']));
     } else {
         // Fino al caso più generico
         console.log(index);
-        let nuovoLi = document.createElement('li');
-        nuovoLi.classList.add('list-group-item', 'list-group-item-primary');
-        nuovoLi.innerHTML = `${index}`;
-        resultDiv.appendChild(nuovoLi);
+        resultDiv.appendChild(createLiElement(`${index}`, ['list-group-item-primary']));
     }
 }
