c2b1c466db87efd6d6edf2f176c5c3a33451cac9
Maisdesign <downloadtaky@gmail.com>
2025-12-10 19:01:28 +0100

    Esercizio completato, avevo dimenticato di committare -.-'

--- js/script.js
+++ js/script.js
@@ -1,13 +1,40 @@
-for (let index = 0; index <100; index = index + 1) {
+const resultDiv = document.getElementById('result');
+
+
+for (let index = 1; index <= 100; index = index + 1) {
     // Parto dal caso più specifico
     if (index % 3 === 0 && index % 5 === 0) {
         console.log("FizzBuzz");
+        let nuovoLi = document.createElement('li');
+        nuovoLi.classList.add('fizzbuzz-item', 'list-group-item');
+        nuovoLi.innerHTML = `<div class="alert alert-danger" role="alert">
+            FizzBuzz
+        </div>`;
+        resultDiv.appendChild(nuovoLi);
     } else if (index % 3 === 0) {
         console.log("Fizz");
+        let nuovoLi = document.createElement('li');
+        nuovoLi.classList.add('fizzbuzz-item', 'list-group-item');
+        nuovoLi.innerHTML = `<div class="alert alert-success" role="alert">
+            Fizz
+        </div>`;
+        resultDiv.appendChild(nuovoLi);
     } else if (index % 5 === 0) {
         console.log("Buzz");
+        let nuovoLi = document.createElement('li');
+        nuovoLi.classList.add('fizzbuzz-item', 'list-group-item');
+        nuovoLi.innerHTML = `<div class="alert alert-info" role="alert">
+            Buzz
+        </div>`;
+        resultDiv.appendChild(nuovoLi);
     } else {
         // Fino al caso più generico
         console.log(index);
-    }   
+        let nuovoLi = document.createElement('li');
+        nuovoLi.classList.add('fizzbuzz-item', 'list-group-item');
+        nuovoLi.innerHTML = `<div class="alert alert-primary" role="alert">
+            ${index}
+        </div>`;
+        resultDiv.appendChild(nuovoLi);
+    }
 }
